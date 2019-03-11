b16a335a25770819383b82f24b9c27e872dbcab3
niexiaobo <357786223@qq.com>
2019-03-11 14:06:22 +0800

    edit2

--- 富文本编辑器/富文本编辑器/ViewController.m
+++ 富文本编辑器/富文本编辑器/ViewController.m
@@ -15,26 +15,24 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    UILabel *text = [[UILabel alloc]initWithFrame:self.view.bounds];
-    text.textAlignment = 1;
-    text.text = @"开始编辑文章";
-    [self.view addSubview:text];
-    
-    
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+
+  UILabel *text = [[UILabel alloc] initWithFrame:self.view.bounds];
+  text.textAlignment = 1;
+  text.text = @"开始编辑文章";
+  [self.view addSubview:text];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    //提前渲染webview
-    [[WGUIWebViewPool sharedInstance] preLoadWebView];
+  [super viewDidAppear:animated];
+  //提前渲染webview
+  [[WGUIWebViewPool sharedInstance] preLoadWebView];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    YSHtmlEditBaseVCtrl *vc = [[YSHtmlEditBaseVCtrl alloc]init];
-    [self.navigationController pushViewController:vc animated:YES];
+  YSHtmlEditBaseVCtrl *vc = [[YSHtmlEditBaseVCtrl alloc] init];
+  [self.navigationController pushViewController:vc animated:YES];
 }
 
 @end
